e63cf8ec0a6268dbca913329cac8d36951822264
circl <circl.lastname@gmail.com>
2022-02-16 15:52:53 +0100

    Use .lisp extension

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ rm -r docs/*
 cp -r src/* docs
 
 function build() {
-  hyperlisp "$1" "$(echo "$1" | sed "s/\\.hl$/.html/")"
+  hyperlisp "$1" "$(echo "$1" | sed "s/\\.lisp$/.html/")"
   rm "$1"
 }
 export -f build
 
-find docs -type f -name "*.hl" -exec bash -c 'build {}' \;
+find docs -type f -name "*.lisp" -exec bash -c 'build {}' \;
