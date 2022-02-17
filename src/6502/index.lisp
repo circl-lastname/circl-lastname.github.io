825bc662587ed27059b1f0f477b2ed172c8fd39b
circl <circl.lastname@gmail.com>
2022-02-17 12:32:30 +0100

    Format the code better

--- src/6502/index.lisp
+++ src/6502/index.lisp
@@ -10,11 +10,13 @@
     (h1 "6502 stuff")
     
     (p "The 6502 is a nice little chip. I forked an "
-      (a :href "https://github.com/circl-lastname/6502js" :target '_blank "online 6502 simulator")
-    ", and I'm messing around with it.")
+         (a :href "https://github.com/circl-lastname/6502js" :target '_blank
+         "online 6502 simulator")
+       ", and I'm messing around with it.")
     
     (p "I use "
-      (a :href "http://sun.hasenbraten.de/vasm/" :target '_blank "vasm")
-    " as my assembler, and a "
-      (a :href "dcbify.js" :target '_blank "node script")
-    " I made to convert the binary data to " (code "dcb") " statements for the online simulator.")))
+         (a :href "http://sun.hasenbraten.de/vasm/" :target '_blank "vasm")
+       " as my assembler, and a "
+         (a :href "dcbify.js" :target '_blank "node script")
+       " I made to convert the binary data to " (code "dcb") " statements for "
+       "the online simulator.")))
--- src/mblf/index.lisp
+++ src/mblf/index.lisp
@@ -9,7 +9,12 @@
     
     (h1 "MBLF and Brainfuck stuff")
     
-    (p "In order to simplify Brainfuck development, I made a language called MBLF. It's assembly-close-enough and has 14 instructions for common tasks in Brainfuck programs. It uses a unique \"pointing\" system, where instructions act upon the currently pointed variable and an additional variable or value provided in an argument, allowing you to chain actions like so:")
+    (p "In order to simplify Brainfuck development, I made a language called "
+       "MBLF. It's assembly-close-enough and has 14 instructions for common "
+       "tasks in Brainfuck programs. It uses a unique \"pointing\" system, "
+       "where instructions act upon the currently pointed variable and an "
+       "additional variable or value provided in an argument, allowing you to "
+       "chain actions like so:")
     
     (pre
 "var a ;; create variable 'a'
@@ -20,14 +25,18 @@ print ;; print the same character
 sub 1 ;; print the previous character in the alphabet/ascii
 print")
     
-    (p "Its original compiler did things pretty badly and was written in JavaScript, it had a few extra instructions like " (code "if") " and " (code "while") ", but I unfortunately removed it.")
+    (p "Its original compiler did things pretty badly and was written in "
+       "JavaScript, it had a few extra instructions like " (code "if") " and "
+       (code "while") ", but I unfortunately removed it.")
     
     (p "My friend "
-      (a :href "https://raymon.dev/" :target '_blank "Raymon")
-    " wrote a new version of the compiler in Rust using Pest grammar. You can find it on "
-      (a :href "https://crates.io/crates/mblf" :target '_blank "crates.io")
-    ".")
+         (a :href "https://raymon.dev/" :target '_blank "Raymon")
+       " wrote a new version of the compiler in Rust using Pest grammar. You "
+       "can find it on "
+         (a :href "https://crates.io/crates/mblf" :target '_blank "crates.io")
+       ".")
     
     (p "I have created a "
-      (a :href "reference.txt" :target '_blank "very rough cheat sheet for the language")
-    ". If you have any problems, feel free to contact me.")))
+         (a :href "reference.txt" :target '_blank "very rough cheat sheet for "
+                                                  "the language")
+       ". If you have any problems, feel free to contact me.")))
