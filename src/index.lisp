1e9b7adf8f450cc4bafdb910c576d797ec837eec
circl <circl.lastname@gmail.com>
2023-07-23 11:39:19 +0200

    Add MSED

--- src/index.lisp
+++ src/index.lisp
@@ -13,6 +13,7 @@
     (h2 "Software projects")
     
     (ul
+      (li (a :href "msed"      "MSED and SDL stuff"))
       (li (a :href "hyperlisp" "Hyperlisp!"))
       (li (a :href "6502"      "6502 stuff"))
       (li (a :href "mblf"      "MBLF and Brainfuck stuff")))
